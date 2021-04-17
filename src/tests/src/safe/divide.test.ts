06cb5db44627f2d7413ab54b5048fd1a4ebe614f
diced <pranaco2@gmail.com>
2021-04-17 13:10:03 -0700

    unit tests

---
+++ src/tests/src/safe/divide.test.ts
@@ -0,0 +1,35 @@
+import { divide } from '../../../safe';
+
+describe('safe divide', () => {
+  test('divide default arg values to equal 1', () => {
+    expect(divide()).toBe(1);
+  });
+
+  test('divides 5 / 2 to equal 2.5', () => {
+    expect(divide(5, 2)).toBe(2.5);
+  });
+
+  test('divides 5 / -2 to equal -2.5', () => {
+    expect(divide(5, -2)).toBe(-2.5);
+  });
+
+  test('divides -5 / 7 to equal -0.7142857142857143', () => {
+    expect(divide(-5, 7)).toBe(-0.7142857142857143);
+  });
+
+  test('divides \'5\' / 2 to equal 2.5', () => {
+    expect(divide('5', 2)).toBe(2.5);
+  });
+
+  test('divides 5 / \'2\' to equal 2.5', () => {
+    expect(divide(5, '2')).toBe(2.5);
+  });
+
+  test('divides \'5\' / \'2\' to equal 2.5', () => {
+    expect(divide('5', '2')).toBe(2.5);
+  });
+
+  test('divides \'5\' / \'-7\' to equal -0.7142857142857143', () => {
+    expect(divide('5', '-7')).toBe(-0.7142857142857143);
+  });
+});
