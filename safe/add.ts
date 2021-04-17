3de912b9cc90d50a1f95e126468740e78cfd7778
Kaihō Dev <82481488+kaihodev@users.noreply.github.com>
2021-04-16 22:58:58 -0400

    Create add.ts

---
+++ safe/add.ts
@@ -0,0 +1,11 @@
+import type { NumberLike } from '../shared/generics.d.ts';
+
+/**
+ * Adds two given NumberLikes.
+ *
+ * @param x - The augend.
+ * @param y - The addend.
+ */
+function add(x: NumberLike = 0, y: NumberLike = 0): number {
+  return +x + +y;
+}
