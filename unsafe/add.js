e021372610c64a7beb2dd4b4ba843d7711d235b9
Kaihō Dev <82481488+kaihodev@users.noreply.github.com>
2021-04-16 23:05:39 -0400

    Create add.js

---
+++ unsafe/add.js
@@ -0,0 +1,13 @@
+/**
+ * Adds two given inputs using the built-in operator+.
+ *
+ * @remarks
+ *
+ * The function makes no assumptions about the input types. 
+ * Number casting is not performed on the augend, so operations including string concatenations will work perfectly, unlike in the safe version.
+ *
+ * @param x - The augend.
+ * @param y - The addend.
+ */
+const add = (x: any, y: any): number => { return x + y; }
+export default add;
