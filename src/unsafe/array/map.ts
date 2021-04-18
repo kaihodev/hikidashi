39917baa8d8fa8eaac652cce3327bebd3f745023
@kaihodev <kaihodev@gmail.com>
2021-04-18 19:37:40 -0400

    Remove less-used thisArg from unsafe map

--- src/unsafe/array/map.ts
+++ src/unsafe/array/map.ts
@@ -9,13 +9,12 @@ import type { ArrayLike, ArrayCB } from '@/shared/generics';
  *
  * @param arr - The input array-like to map through.
  * @param cb  - The callback provided to produce mapped results. This iteratee is invoked with three arguments: value, index|key, and arr.
- * @param t   - The value to use as `this` when executing callback.
  *
  * @returns unknown[] - The new mapped array.
  */
-const map = (arr: ArrayLike, cb: ArrayCB, t: unknown = undefined): unknown[] => {
+const map = (arr: ArrayLike, cb: ArrayCB): unknown[] => {
   // eslint-disable-next-line no-var
-  for (var i = arr.length, result = Array(i); i--; result[i] = cb.call(t, arr[i], i, arr));
+  for (var i = arr.length, result = Array(i); i--; result[i] = cb(arr[i], i, arr)); // eslint-disable-line callback-return
   return result;
 };
 export default map;
