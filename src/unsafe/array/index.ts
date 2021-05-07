ff812f850a878866e5eb4d633044df101e109ab5
@kaihodev <kaihodev@gmail.com>
2021-05-07 19:16:04 -0400

    feat(unsafe): export left/right variants of iterative functions

--- src/unsafe/array/index.ts
+++ src/unsafe/array/index.ts
@@ -1,16 +1,33 @@
 export { default as map } from './map';
+export const mapLeft = null;
+export { default as mapRight } from './map';
 export { default as mapip } from './mapip';
+export const mapipLeft = null;
+export { default as mapipRight } from './mapip';
 
 export { default as each } from './each';
 export { default as forEach } from './each';
+export const eachLeft = null;
+export { default as eachRight } from './each';
 
 export { default as filter } from './filter';
+export { default as filterLeft } from './filter';
+export const filterRight = null;
 
 export { default as reduce } from './reduce';
+export { default as reduceLeft } from './reduce';
+export const reduceRight = null;
 
 export { default as partition } from './partition';
+export { default as partitionLeft } from './partition';
+export const partitionRight = null;
 
 export { default as isArithmeticSequence } from './isArithmeticSequence';
 
 export { default as indexOf } from './indexOf';
+export { default as indexOfLeft } from './indexOf';
+export const indexOfRight = null;
+
 export { default as includes } from './includes';
+export { default as includesLeft } from './includes';
+export const includesRight = null;
