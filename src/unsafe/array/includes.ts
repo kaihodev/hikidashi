267815333620edcfeeca390b9abba88e1ea603a9
rpxs <me@rpxs.me>
2021-05-10 07:46:40 -0600

    Update includes.ts

--- src/unsafe/array/includes.ts
+++ src/unsafe/array/includes.ts
@@ -2,6 +2,14 @@ import type { ArrayLike } from '@/shared/generics';
 import indexOf from './indexOf';
 
 /**
- * @todo
+ * A non-spec compliant utility function to check if an Array includes something.
+ *
+ * @remarks
+ *
+ * @param arr - The array-like input to iterate through.
+ * @param element - The item to search for in the array.
+ * @param fromIndex - The index of the array to start searching from.
+ *
+ * @returns void
  */
 export default function includes(arr: ArrayLike, element: any, fromIndex = 0): boolean { return indexOf(arr, element, fromIndex) !== -1; }
