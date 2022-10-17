6e58b5760d9f2bc4581a1b8ea59f897fd8a9e2b9
kaihodev <kaihodev@gmail.com>
2022-10-17 20:07:25 +0000

    Deploy canary - based on 91405279cae2efb03ea34f1047c886ec2c825ff7

---
+++ unsafe/matrix/msubeq.d.ts
@@ -0,0 +1,17 @@
+import type { ArrayLike } from '@/shared/generics';
+/**
+ * Subtracts two arrays back into and *modifying the first array* as if a -= b.
+ *
+ * @remarks
+ *
+ * This implementation loops in reverse order.
+ * The maddeq function should be used with caution due to its destructive nature.
+ * We implicitly assume that the second array contains at least as many elements as the first.
+ *
+ * @param a - The minuend, first input array-like to subtract and modify.
+ * @param b - The subtrahend, second input array-like to subtract.
+ *
+ * @returns typeof a - The modified input.
+ */
+declare const msubeq: (a: ArrayLike, b: ArrayLike) => any;
+export default msubeq;
