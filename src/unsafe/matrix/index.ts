7002c7c5fce6a45bc668058a9142dc7681533f96
William Zhou <wzhouwzhou@gmail.com>
2022-10-17 22:44:49 -0400

    feat(unsafe): add mmuleq and mdiveq, export matrix (#79)
    * feat(unsafe): add msubeq
    
    * feat(unsafe): add mmuleq and mdiveq, export matrix
    
    * build(config): add vsc launch config

---
+++ src/unsafe/matrix/index.ts
@@ -0,0 +1,4 @@
+export { default as maddeq } from './maddeq';
+export { default as msubeq } from './msubeq';
+export { default as mmuleq } from './mmuleq';
+export { default as mdiveq } from './mdiveq';
